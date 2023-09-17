3ec292f470e1cef9c81309e1c696e0c9c4f35244
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-09-17 22:31:39 +0900

    Update 02.c

--- week2/02.c
+++ week2/02.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+    int a, b;
+    a = 10;
+    b = 20;
+
     printf("Hello, Sookmyung Electronics!\n");
+    printf("%d + %d = %d\n", a, b, a+b);
     return 0;
 }
